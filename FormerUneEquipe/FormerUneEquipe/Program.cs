2cc451ea5a04ef3c20a12d2611638508a29bf208
Med-NAIMI <56475732+Med-NAIMI@users.noreply.github.com>
2021-04-25 19:18:49 +0200

    Add files via upload

---
+++ FormerUneEquipe/FormerUneEquipe/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormerUneEquipe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string continu = "oui"; //controler la saisie
+            int i = 1;//numerotation des joueurs
+            List<int> S = new List<int>();//la liste contenant les notes de joueurs
+            int input;//variable intermédiaire pour la saisie
+            while (continu=="oui")//la saisie se termine lorsque l utilisateur entre une valeur d'autre que "oui"
+            {
+                Console.WriteLine("entrez la note du joueur n° "+i);
+                input = Convert.ToInt32(Console.ReadLine());
+                S.Add(input);
+
+                Console.WriteLine("entrez oui pour continuer la saisie des notes");
+                continu= Console.ReadLine();
+                i++;
+            }
+
+            //la saisie du nbr de joueurs au minimum par équipe
+            int M;
+            Console.WriteLine("entrez le nbr minimale de joueurs formant une equipe ");
+            M = Convert.ToInt32(Console.ReadLine());
+
+            //la saisie de la note minimale incluse requise pour sélectionner un joueur
+            int L;
+            Console.WriteLine("entrez la note minimale de joueurs acceptés");
+            L = Convert.ToInt32(Console.ReadLine());
+
+            //la saisie de la note maximale incluse requise pour sélectionner un joueur
+            int U;
+            Console.WriteLine("entrez la note maximale de joueurs acceptés");
+            U = Convert.ToInt32(Console.ReadLine());
+
+            int nbrEquipe=Traitement.nbrEqupuipeForme(S, M, L, U);
+            Console.WriteLine("le nbr d'equipes est: "+nbrEquipe);
+
+            Console.Read();
+            Console.ReadKey();
+            string c = Console.ReadLine();//pour conserver l'affichage
+        }
+    }
+}
---
+++ FormerUneEquipe/FormerUneEquipe/Traitement.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormerUneEquipe
+{
+    abstract class Traitement
+    {
+        public static int nbrEqupuipeForme(List<int> S,int M,int L, int U)
+        {
+            //selectionner les jooeurs qui verifient les conditions requises
+            var joueursAccepte = S.Where(p => p >= L && p <= U).ToList();
+            int N = M;//le nbr de joueurs pouvant former une equipe
+            int nbrEquipe = 0;//le nbr d'equipe qu'onpeu former suivant les criteres données
+            while(N<=joueursAccepte.Count)
+            {
+                //ajouter le nbr de combinaison pour N joeurs dans l'equipe
+                nbrEquipe +=factorielle(joueursAccepte.Count)/(factorielle(N)*factorielle(joueursAccepte.Count-N));
+
+                N++;//incrementer le nbr de joueurs par equipe 
+            }
+
+            return nbrEquipe;
+        }
+
+        private static int factorielle(int n)
+        {
+            int p = 1;
+            for (int i=1; i <= n; i++)
+                p*=i;
+            return p;
+        }
+    }
+}
